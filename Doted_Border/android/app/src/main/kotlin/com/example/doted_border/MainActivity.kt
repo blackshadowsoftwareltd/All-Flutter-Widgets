946c5fa1cdfa86be2ccab2a29b147a74ef075d26
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2021-07-24 19:52:55 +0600

    doted border

---
+++ Doted_Border/android/app/src/main/kotlin/com/example/doted_border/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doted_border
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Doted_Border/lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            dashedBorder1(),
+            dashedBorder2(),
+            dashedBorder3(),
+            dashedBorder4(),
+          dashedBorder5(),dashedBorder6(),]),
+        ),
+      ),
+    );
+  }
+
+  Widget dashedBorder1() => DottedBorder(
+      color: Colors.red,
+      strokeWidth: 4,
+      radius: Radius.circular(20),
+      borderType: BorderType.RRect,
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        // color: Colors.green,
+        child: Text('DottedBorder'),
+      ));
+  Widget dashedBorder2() => DottedBorder(
+      color: Colors.red,
+      strokeWidth: 4,
+      borderType: BorderType.Circle,
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        // color: Colors.green,
+        child: Text('DottedBorder'),
+      ));
+  Widget dashedBorder3() => DottedBorder(
+      color: Colors.red,
+      strokeWidth: 4,
+      borderType: BorderType.Rect,
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        // color: Colors.green,
+        child: Text('DottedBorder'),
+      ));
+  Widget dashedBorder4() => DottedBorder(
+      color: Colors.red,
+      strokeWidth: 4,
+      borderType: BorderType.Circle,
+      dashPattern: [20, 5],
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        // color: Colors.green,
+        child: Text('DottedBorder'),
+      ));
+  Widget dashedBorder5() => DottedBorder(
+      color: Colors.red,
+      strokeWidth: 4,
+      borderType: BorderType.Circle,
+      dashPattern: [15, 10,5,10],
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        // color: Colors.green,
+        child: Text('DottedBorder'),
+      ));
+  Widget dashedBorder6() => DottedBorder(
+      color: Colors.red,
+      strokeWidth: 10,
+      strokeCap: StrokeCap.round,
+      borderType: BorderType.Circle,
+      dashPattern: [5, 20],
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        width: 100,
+        // color: Colors.green,
+        child: Text('DottedBorder'),
+      ));
+}
