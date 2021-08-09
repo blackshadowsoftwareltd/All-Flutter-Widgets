4d401452a5a6821119de69000817b9fff2dbe1bc
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2021-08-09 23:16:07 +0600

    Forced update

---
+++ Forced_update/android/app/src/main/kotlin/com/example/forced/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.forced
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Forced_update/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:new_version/new_version.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    checkVersion();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  void checkVersion() async {
+    final newVersion = NewVersion(
+      androidId: 'com.blackshadowsoftware.ktmwallpaper',
+    );
+
+    /// only notify the users.
+    // newVersion.showAlertIfNecessary(context: context);
+
+    final status = await newVersion.getVersionStatus();
+    newVersion.showUpdateDialog(
+      context: context,
+      versionStatus: status!,
+      dialogTitle: 'Please Update',
+      dialogText:
+          'Your App needs an update. you must need to update this App to use.\nCurrent version is ${status.localVersion}. Store version is ${status.storeVersion}',
+
+      ///
+      updateButtonText: 'Update now',
+
+      ///
+      dismissButtonText: 'Close App',
+      // allowDismissal: false
+      dismissAction: () => SystemNavigator.pop(),
+    );
+
+    print(
+        'Local version: ${status.localVersion} Store version: ${status.storeVersion}');
+  }
+}
