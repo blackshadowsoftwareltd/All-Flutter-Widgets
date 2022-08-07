7f1cd9927b9e614f0f584a7180ffdfd61077c1c5
remon <blackshadowsoftwareltd@gmail.com>
2022-08-07 22:19:03 +0600

    animation in shader mask

--- shader_mask_gradient_text/lib/main.dart
+++ shader_mask_gradient_text/lib/main.dart
@@ -2,26 +2,61 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  List<Color> colors = List<Color>.generate(
+      8,
+      (index) =>
+          index.isOdd ?  Colors.blue :   Colors.green);
+  List<double> stops = List<double>.generate(8, (index) => index * 0.2 - 0.4);
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+
+    animation = Tween<double>(begin: .0, end: .4).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reset();
+          controller.forward();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+    controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
           child: ShaderMask(
-        shaderCallback: (bounds) => const LinearGradient(colors: [
-          Colors.orange,
-          Colors.green,
-          Colors.blue,
-          Colors.red,
-          Colors.purple,
-          Colors.blueGrey
-        ]).createShader(bounds),
+        shaderCallback: (bounds) => SweepGradient(
+                colors: colors,
+                stops: stops.map((s) => s + animation.value).toList())
+            .createShader(bounds),
         child: const Text(
-          'REMON AHAMMAD',
+          'REMON\nAHAMMAD',
+          textAlign: TextAlign.center,
           style: TextStyle(
-              color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold),
+              color: Colors.white,
+              fontSize: 50,
+              fontWeight: FontWeight.w900,
+              height: .9),
         ),
       )),
     );
