034978100e2a50ef25d3607ea02b4e48d0cf2745
Remon <blackshadowsoftwareltd@gmail.com>
2023-01-02 11:42:46 +0600

    colors

--- colors_c/lib/main.dart
+++ colors_c/lib/main.dart
@@ -1,115 +1,179 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Material Color Shades'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            PrimaryColorOptions(),
+            MaterialColorShades(),
+            PrimaryColorOptions(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class PrimaryColorOptions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueGrey, width: 2),
+      ),
+      child: Row(
+        children: List.generate(
+          AppColors.primaryColors.length,
+          (i) => Expanded(
+            child: Container(
+              height: 50,
+              color: AppColors.primaryColors[i],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MaterialColorShades extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 500,
+      child: Row(
+        children: List.generate(
+          AppColors.materialColors.length,
+          (i) {
+            return Expanded(
+              child: Column(
+                children: _buildShadesContainers(AppColors.materialColors[i]),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildShadesContainers(MaterialColor materialColor) {
+    List<Widget> _shadesContainers = [];
+    AppColors.getMaterialColorShades(materialColor).forEach(
+      (materialColorShadeIndex, materialColorShade) {
+        _shadesContainers.add(Expanded(
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: materialColorShade,
+              border: materialColorShadeIndex == 500
+                  ? const Border(
+                      top: BorderSide(width: 2, color: Colors.blueGrey),
+                      bottom: BorderSide(width: 2, color: Colors.blueGrey),
+                    )
+                  : null,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (materialColorShadeIndex == 500) const Text('MAIN'),
+                Text('$materialColorShadeIndex'),
+              ],
+            ),
+          ),
+        ));
+      },
+    );
+    return _shadesContainers;
+  }
+}
+
+class AppColors {
+  static List<String> hexColors = [
+    'd23156',
+    '16b9fd',
+    '13d0c1',
+    'e5672f',
+    'b73d99',
+  ];
+
+  static List<Color> get primaryColors {
+    List<Color> _primaryColors = [];
+
+    for (String hexColor in hexColors) {
+      Color _color = Color(int.parse('0xff$hexColor'));
+      _primaryColors.add(_color);
+    }
+
+    return _primaryColors;
+  }
+
+  static Color getShade(Color color, {bool darker = false, double value = .1}) {
+    assert(value >= 0 && value <= 1);
+
+    final hsl = HSLColor.fromColor(color);
+    final hslDark = hsl.withLightness(
+        (darker ? (hsl.lightness - value) : (hsl.lightness + value))
+            .clamp(0.0, 1.0));
+
+    return hslDark.toColor();
+  }
+
+  static MaterialColor getMaterialColorFromColor(Color color) {
+    Map<int, Color> _colorShades = {
+      50: getShade(color, value: 0.5),
+      100: getShade(color, value: 0.4),
+      200: getShade(color, value: 0.3),
+      300: getShade(color, value: 0.2),
+      400: getShade(color, value: 0.1),
+      500: color,
+      600: getShade(color, value: 0.1, darker: true),
+      700: getShade(color, value: 0.15, darker: true),
+      800: getShade(color, value: 0.2, darker: true),
+      900: getShade(color, value: 0.25, darker: true),
+    };
+    return MaterialColor(color.value, _colorShades);
+  }
+
+  static List<MaterialColor> get materialColors {
+    List<MaterialColor> _materialColors = [];
+    for (Color primaryColor in primaryColors) {
+      _materialColors.add(getMaterialColorFromColor(primaryColor));
+    }
+    return _materialColors;
+  }
+
+  static Map<int, Color> getMaterialColorShades(MaterialColor materialColor) {
+    return {
+      50: materialColor.shade50,
+      100: materialColor.shade100,
+      200: materialColor.shade200,
+      300: materialColor.shade300,
+      400: materialColor.shade400,
+      500: materialColor.shade500,
+      600: materialColor.shade600,
+      700: materialColor.shade700,
+      800: materialColor.shade800,
+      900: materialColor.shade900,
+    };
   }
 }
